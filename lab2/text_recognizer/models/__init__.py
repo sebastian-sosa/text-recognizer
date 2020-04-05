e521adfb34075676a3e5e285ea3f3916af666f8f
Sebastian <s.sosarippe@gmail.com>
2020-04-05 14:49:48 +0000

    Delete unfinished projects

--- lab2/text_recognizer/models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Model modules."""
-from .character_model import CharacterModel
-
-# Hide lines below until Lab 2
-from .line_model import LineModel
-from .line_model_ctc import LineModelCtc
-
-# Hide lines above until Lab 2
