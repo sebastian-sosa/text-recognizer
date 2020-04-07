458a76e1f86ab913012e597b06ba36676c395703
Sebastian <s.sosarippe@gmail.com>
2020-04-07 18:13:21 +0000

    Fix docker build script

--- lab8/tasks/build_api_docker.sh
+++ lab8/tasks/build_api_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed 's/tensorflow==/tensorflow-cpu==/' requirements.txt > api/requirements.txt
+sed 's/tensorflow==/tensorflow-cpu==/' ../requirements.txt > ./api/requirements.txt
 
 docker build -t text_recognizer_api -f api/Dockerfile .
